4553fa65c822b4bf1cf41079136e6bcbcf501da2
CB_X2_Jun <zzh20120518@outlook.com>
2026-08-09 21:32:39 +0800

    Update dns.js

--- functions/api/dns.js
+++ functions/api/dns.js
@@ -1,41 +1,63 @@
 const DOMAIN="proxyip.etoj.run.place";
 const TYPES=["","-res","-edu","-mob"];
-const COUNTRIES="AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW".split(" ");
+
+const COUNTRIES="AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CY CZ DE DJ DK DM DO DZ EC EE EG ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW".split(" ");
 
 async function resolve(name){
-    try{
-        const r=await fetch("https://1.1.1.1/dns-query?"+new URLSearchParams({name,type:"A"}),{
-            headers:{accept:"application/dns-json"}
-        });
-        const j=await r.json();
-        return (j.Answer||[]).filter(x=>x.type===1).map(x=>x.data);
-    }catch(e){
-        return [];
+  try{
+    const url="https://cloudflare-dns.com/dns-query?"+new URLSearchParams({
+      name,
+      type:"A"
+    });
+
+    const r=await fetch(url,{
+      headers:{
+        accept:"application/dns-json"
+      }
+    });
+
+    if(!r.ok){
+      console.log("DNS HTTP error",name,r.status);
+      return [];
     }
+
+    const j=await r.json();
+
+    return (j.Answer||[])
+      .filter(x=>x.type===1)
+      .map(x=>x.data);
+
+  }catch(e){
+    console.log("DNS error",name,e);
+    return [];
+  }
 }
 
 export async function onRequest(){
-    const tasks=[];
-    for(const c of COUNTRIES){
-        for(const t of TYPES){
-            const domain=`${c.toLowerCase()}${t}.${DOMAIN}`;
-            tasks.push(
-                resolve(domain).then(ips=>{
-                    if(!ips.length)return null;
-                    return {
-                        domain,
-                        country:c,
-                        type:t.replace("-","")||"dc",
-                        ips
-                    };
-                })
-            );
-        }
+  const data=[];
+
+  for(const c of COUNTRIES){
+    for(const t of TYPES){
+
+      const domain=`${c.toLowerCase()}${t}.${DOMAIN}`;
+
+      const ips=await resolve(domain);
+
+      if(ips.length){
+        data.push({
+          domain,
+          country:c,
+          type:t? t.slice(1):"dc",
+          ips
+        });
+      }
+
     }
-    const data=(await Promise.all(tasks)).filter(Boolean);
-    return Response.json(data,{
-        headers:{
-            "Cache-Control":"public,max-age=300"
-        }
-    });
+  }
+
+  return Response.json(data,{
+    headers:{
+      "Cache-Control":"public,max-age=300"
+    }
+  });
 }
